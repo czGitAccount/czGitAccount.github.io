181a69d6b4c150fcda8dfcd91a84e5496e36fe4d
zhi_cao <731987255@qq.com>
2021-06-19 21:20:20 +0800

    Site updated: 2021-06-19 21:20:19

--- js/src/highlight-wrap.js
+++ js/src/highlight-wrap.js
@@ -1,3 +1,3 @@
-// build time:Sat Jun 19 2021 21:16:55 GMT+0800 (GMT+08:00)
+// build time:Sat Jun 19 2021 21:20:16 GMT+0800 (GMT+08:00)
 function initMacPanel(){var a=$(".highlight").not(".gist .highlight");for(var t=0;t<a.length;t++){var e=$(a[t]);var i=e.attr("class");var r=/highlight(\s?)([a-zA-Z]*)/;r.test(i);var n=RegExp.$2||"plain";var o=$("<div>").attr("data-rel",n.toUpperCase()).addClass("highlight-wrap");var c=e.wrap(o);c.before('<button class="copy-btn" data-clipboard-snippet=""><i class="fa fa-clipboard"></i></button>')}}function initClipboard(){var a=new ClipboardJS(".copy-btn",{target:function(a){var t=a.parentElement;var e=t.querySelector(".code");return e}});a.on("success",function(a){var t=a.trigger;var e=t.querySelector("i");e.className="fa fa-check";t.onmouseleave=function(t){a.clearSelection();setTimeout(function(){e.className="fa fa-clipboard"},1e3)}})}$(document).ready(function(){initMacPanel();initClipboard()});
 //rebuild by neat 
