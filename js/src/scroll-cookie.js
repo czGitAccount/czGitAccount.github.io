8a1d725a7ec2337902290d49ed34ac7c13898e34
zhi_cao <731987255@qq.com>
2021-08-09 10:43:58 +0800

    Site updated: 2021-08-09 10:43:55

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Thu Aug 05 2021 20:28:29 GMT+0800 (GMT+08:00)
+// build time:Mon Aug 09 2021 10:43:52 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
